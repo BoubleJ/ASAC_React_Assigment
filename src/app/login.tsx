1b242233eda5df4ac9a5e45d0d3046c9e54ee9c2
jaejungbyun <dogmnil2007@naver.com>
2023-08-30 17:06:59 +0900

    useState + useRef/ sign up

---
+++ src/app/login.tsx
@@ -0,0 +1,181 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState, useRef } from 'react';
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const emailInputRef = useRef<HTMLInputElement | null>(null);
+  const passwordInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    setEmail(data.get('email'));
+    setPassword(data.get('password'));
+    handleSignUp();
+  };
+
+  const handleSignUp = () => {
+    // 이곳에서 email과 password의 유효성을 검사하세요.
+    if (!isValidEmail(email)) {
+      alert('이메일 형식이 올바르지 않습니다.');
+      if (emailInputRef.current) {
+        emailInputRef.current.focus();
+      }
+    } else if (!isValidPassword(password)) {
+      alert('비밀번호 형식이 올바르지 않습니다.');
+      if (passwordInputRef.current) {
+        passwordInputRef.current.focus();
+      }
+    } else {
+      // 회원 가입 로직을 처리하세요.
+      console.log('success');
+    }
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    // 이메일 유효성 검사 로직을 구현하세요.
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return emailRegex.test(email);
+  };
+
+  const isValidPassword = (password: string): boolean => {
+    // 패스워드 유효성 검사 로직을 구현하세요.
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,118 +1,17 @@
-'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
-
+'use client';
+import Image from 'next/image';
+import SignUp from './login';
+import { Stack, Button } from '@mui/material';
+import AbcIcon from '@mui/icons-material/Abc';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import IconButton from '@mui/material/IconButton';
 import { useState } from 'react';
 
-
-function Btn(props: { title: string, bg_color: string, color: string }) {
-  return (
-    <button
-    className={styles.snsBtn}
-      style={{ backgroundColor: props.bg_color, color: props.color }}
-    >
-     
-      {props.title}로 로그인
-    </button>
-  );
-}
-
-
-
-function Input (props) {
-  return (
-    <div className={styles.login_input}>
-    <input
-      type={props.type}
-      name={props.name}
-      placeholder={props.placeholder}
-     
-    />
-  </div>
-
-  )
-}
-
-
-
-
 export default function Home() {
-
-  let [LoginSNS, setLoginSNS] = useState<string[]>([" 카카오톡으", " Google"]);
-
-
-
-
-
   return (
-    <main className={styles.background}>
- <section>
-        <form>
-          <input type="hidden"></input>
-          <div className={styles.title_logo}>
-            <strong className={styles.logo}>
-              <a href="#">여기어때</a>
-            </strong>
-          </div>
-
-          <div className={styles.snsBtn_box}>
-            <Btn
-              title={LoginSNS[0]}
-              bg_color="rgb(252,229,30)"
-              color="black"
-            ></Btn>
-            <Btn
-              title={LoginSNS[1]}
-              bg_color="rgb(24,119,242)"
-              color="white"
-            ></Btn>
-            
-          </div>
-
-          <div className={styles.or_box}>
-            <p className={styles.space_or}>
-              <span>또는</span>
-            </p>
-          </div>
-
-          <Input 
-          type="email"
-      name="id"
-      placeholder="이메일 주소"
-   ></Input>
-
-
-      <Input
-      type="password"
-  name="pw"
-  placeholder="비밀번호"
-></Input>
-         
-          <div className={`${styles.snsBtn_box} ${styles.login_btn}`}>
-            <button className={styles.snsBtn} type="submit">
-              로그인
-            </button>
-          </div>
-
-          <div className={styles.footer_box}>
-            <div className={styles.join_box}>
-              <div className={styles.textbox}>
-                <a href="https://www.goodchoice.kr/user/join">
-                  <span>비밀번호 재설정</span>
-                </a>
-              </div>
-            </div>
-
-            <div className={styles.join_box}>
-              <div className={styles.textbox}>
-                <a href="https://www.goodchoice.kr/user/join">
-                  <span>회원가입</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </form>
-      </section>
-    </main>
-  )
+    <div>
+      <SignUp></SignUp>
+    </div>
+  );
 }
