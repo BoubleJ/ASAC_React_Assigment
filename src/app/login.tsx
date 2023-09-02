1b6f16f6bf9c63d8cdb8fd30b71d4f36a13a5acf
jaejungbyun <dogmnil2007@naver.com>
2023-09-02 22:57:52 +0900

    유효성 확인별 모달창 띄우기 성공

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -13,10 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState, useRef } from 'react';
-import { Mail } from '@mui/icons-material';
-import ModalProvider, { ModalContext } from './modal';
-import { useContext } from 'react';
-import { useRouter } from 'next/navigation';
+import Modal2 from './Modal2';
 
 function Copyright(props: any) {
   return (
@@ -39,30 +36,37 @@ function Copyright(props: any) {
 const theme = createTheme();
 
 export default function SignUp() {
-  const { show, hide } = useContext(ModalContext);
-  const router = useRouter();
-  const [modaldescription, setmodaldescription] = useState();
+  const [SignUpModalOn, setSignUpModalOn] = useState(false);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const emailInputRef = useRef<HTMLInputElement | null>(null);
   const passwordInputRef = useRef<HTMLInputElement | null>(null);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalDescription, setModalDescription] = useState('');
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!isValidEmail(email)) {
       console.log(email);
-      alert('이메일 형식이 올바르지 않습니다.');
-
       emailInputRef.current.focus();
-      setmodaldescription('이메일 형식이 올바르지 않습니다.');
+      alert('이메일 형식이 올바르지 않습니다.');
+      setModalTitle('경고');
+      setModalDescription('이메일 형식에 맞지 않습니다.');
+      setSignUpModalOn(true);
     } else if (!isValidPassword(password)) {
       console.log(password);
-      alert('비밀번호 형식이 올바르지 않습니다.');
       passwordInputRef.current.focus();
-      setmodaldescription('비밀번호 형식이 올바르지 않습니다.');
+      alert('비밀번호 형식이 올바르지 않습니다.');
+      setModalTitle('오류');
+      setModalDescription('비밀번호 형식에 맞지 않습니다.');
+      setSignUpModalOn(true);
     } else {
       // 회원 가입 성공
       console.log('success');
+      setModalTitle('축하');
+      setModalDescription('회원가입에 성공했습니다.');
+      setSignUpModalOn(true);
     }
   };
 
@@ -80,138 +84,115 @@ export default function SignUp() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
+    <>
+      <Modal2
+        open={SignUpModalOn}
+        onClose={() => setSignUpModalOn(false)}
+        title={modalTitle}
+        description={modalDescription}
+      ></Modal2>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={e => {
-              handleSubmit(e);
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
-            sx={{ mt: 3 }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  inputRef={emailInputRef}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  inputRef={passwordInputRef}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => {
-                setEmail(emailInputRef.current.value);
-                setPassword(passwordInputRef.current.value);
-
-                // 클릭시 모달창 생성
-                // 왜냐 NestedReactComponent가 부모 컴포넌트인 ModalContext.Provider로 부터
-                // show, hide 함수를 받았는데 show함수 프로퍼티 중 하나인 show는 true니까
-                // 밑의 설정은 켜기버튼 A,B,C,D 모두 동일하게 적용할 수 있다. show 함수 파라미터조작하면 된다.
-                show({
-                  // 클릭시 show 함수를 실행한다 -> show : true를 실행한다.
-                  //modal-provider 파일 내부의 ModalContext.Provider value에 담겨진 show의 파라미터인 setmodal을 가져온다.
-                  title: '경고',
-                  //모달창 제목
-                  description: '후 왜 안뜨냐',
-                  //모달창 내용
-
-                  confirmButton: '확인',
-                  //모달창 확인버튼
-                  //cancelButton, confirmButton 값이 undefined거나 null값이면
-                  // modal-provider에 적혀있는 기본 이름으로 버튼값이정해진다
-                  // {modal.confirmButton ?? '확인'} 이거 말하는거
-                  cancelButton: '취소',
-                  //모달창 취소버튼
-
-                  confirmCallback: () => {
-                    router.push('/');
-                    // 아직 이동할 지정 페이지가 없으니 '/' 로 설정한 것
-                  },
-                  useCancelButton: true,
-                  useConfirmButton: true,
-                  // 취소버튼 사용 유무 결정
-                  // useConfirmButton 이것도 똑같이 사용 가능 (사용유무 결정 가능)
-                });
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={e => {
+                handleSubmit(e);
               }}
+              sx={{ mt: 3 }}
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    inputRef={emailInputRef}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    id="password"
+                    autoComplete="new-password"
+                    inputRef={passwordInputRef}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                  setEmail(emailInputRef.current.value);
+                  setPassword(passwordInputRef.current.value);
+                }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
 'use client';
 import Image from 'next/image';
 import SignUp from './login';
-import { Stack, Button } from '@mui/material';
-import AbcIcon from '@mui/icons-material/Abc';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-import IconButton from '@mui/material/IconButton';
-import { useContext, useState, createContext } from 'react';
-import Detail from './modal';
-
-import ModalProvider, { ModalContext } from './modal';
-
-import { useRouter } from 'next/navigation';
+import Modal2 from './Modal2';
 
 export default function Home() {
-  const { show, hide } = useContext(ModalContext);
-  const router = useRouter();
-
   return (
     <div>
-      <ModalProvider>
-        <SignUp></SignUp>
-      </ModalProvider>
+      <SignUp></SignUp>
+
+      <Modal2></Modal2>
     </div>
   );
 }
